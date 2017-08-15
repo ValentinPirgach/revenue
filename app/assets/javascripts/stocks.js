6124e841a0c887ed893ebf7c5383f741a0589315
Valentin Pirgach <valentyn.pirhach@customertimes.com>
2017-08-15 22:32:31 +0300

    Fix js

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -1,15 +1,15 @@
-let initStockLookup = function () {
+var initStockLookup = function () {
   $('#stock-lookup-form').on('ajax:success', function (event, data, status) {
     $('#stock-lookup').replaceWith(data);
 
     initStockLookup();
-  })
+  });
 
   $('#stock-lookup-form').on('ajax:error', function (event, xhr, status, error) {
     $('#stock-lookup-results').replaceWith('');
     $('#stock-lookup-errors').replaceWith('Stock was not found');
     hideSpinner();
-  })
+  });
 
   $('#stock-lookup-form').on('ajax:before', showSpinner);
   $('#stock-lookup-form').on('ajax:after', hideSpinner);
