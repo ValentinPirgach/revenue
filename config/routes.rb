5c76b61846ef797ea7909cb225efbfc407ca8f47
Valentin Pirgach <valentyn.pirhach@customertimes.com>
2017-08-17 14:14:58 +0300

    Add user page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
 
   resources :user_stocks, except: [:update, :show, :edit]
   resources :users, only: [:show]
-  resources :friendships, only: [:show]
+  resources :friendships, only: [:show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
